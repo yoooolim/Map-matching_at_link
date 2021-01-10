9b8ef775f87f770ee7383e9430867f550130df1c
Yunhye-Choi <anffl98@gmail.com>
2021-01-11 00:02:11 +0900

    general viterbi (유네 tp)로 개발 완료

--- src/Main.java
+++ src/Main.java
@@ -183,9 +183,9 @@ public class Main {
 
         ///////////// FSW VITERBI /////////////
         // window size 입력받기
-        System.out.print("Fixed Sliding Window Viterbi. Window size: \n");
+        System.out.print("Fixed Sliding Window Viterbi. Window size: 3\n");
         //Scanner scanner = new Scanner(System.in);
-        int wSize = 15; // window size;
+        int wSize = 3; // window size;
 
         ArrayList<Candidate[]> subpaths = new ArrayList<>();
         // arrOfCandidates를 순회하며 찾은 path의 마지막을 matching_success에 추가하는 loop
